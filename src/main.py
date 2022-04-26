c2dcdaa6a05bf51ebe7bb32d4966af05276f2a68
pmayaduque <pmayaduque@gmail.com>
2022-04-26 17:19:09 -0500

    create folders

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 17:17:52 2022
+
+@author: pmayaduque
+"""
+
---
+++ src/opt.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 17:17:59 2022
+
+@author: pmayaduque
+"""
+
